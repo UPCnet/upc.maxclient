44e20c2c406ab57ba433b019f52e7545265c6dc1
Carles Bruguera <carlesba@gmail.com>
2012-02-20 08:49:10 +0100

    Adjust package settings

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import os
 version = '1.0'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
     'Contributors\n'
     '============\n'
     + '\n' +
     open('CONTRIBUTORS.txt').read()
     + '\n' +
-    open('docs/CHANGES.txt').read()
+    open('docs/HISTORY.txt').read()
     + '\n')
 
 setup(name='upc.maxclient',
@@ -28,8 +28,7 @@ setup(name='upc.maxclient',
       author_email='',
       url='http://svn.plone.org/svn/collective/',
       license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['upc'],
       include_package_data=True,
       zip_safe=False,
